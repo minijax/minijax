a94b13f0f9272f86e12553cd06d24645ce413fc2
Cody Ebberson <cody@ebberson.com>
2017-10-12 11:53:30 -0700

    Archetype

---
+++ minijax-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+package $package;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.minijax.test.MinijaxTest;
+
+public class AppTest extends MinijaxTest {
+
+    @Before
+    public void setUp() {
+        register(App.class);
+    }
+
+    @Test
+    public void testHello() {
+        assertEquals("Hello world!", target("/").request().get(String.class));
+    }
+}
