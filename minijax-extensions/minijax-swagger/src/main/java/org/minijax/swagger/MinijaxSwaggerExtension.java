3176b882c47e5789ed55ca49c24959b350dd944d
Cody Ebberson <cody@ebberson.com>
2017-11-01 07:46:21 -0700

    Fixed checkstyle errors

--- minijax-extensions/minijax-swagger/src/main/java/org/minijax/swagger/MinijaxSwaggerExtension.java
+++ minijax-extensions/minijax-swagger/src/main/java/org/minijax/swagger/MinijaxSwaggerExtension.java
@@ -3,6 +3,7 @@ package org.minijax.swagger;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -26,22 +27,28 @@ public class MinijaxSwaggerExtension extends AbstractSwaggerExtension {
     final ObjectMapper mapper = Json.mapper();
 
     @Override
-    public List<Parameter> extractParameters(final List<Annotation> annotations, final Type type, final Set<Type> typesToSkip, final Iterator<SwaggerExtension> chain) {
-        List<Parameter> parameters = new ArrayList<Parameter>();
+    public List<Parameter> extractParameters(
+            final List<Annotation> annotations,
+            final Type type,
+            final Set<Type> typesToSkip,
+            final Iterator<SwaggerExtension> chain) {
 
         if (shouldIgnoreType(type, typesToSkip)) {
-            return parameters;
+            return Collections.emptyList();
         }
+
+        final List<Parameter> parameters = new ArrayList<Parameter>();
+
         for (final Annotation annotation : annotations) {
-            // just handle the jersey specific annotation
             if (annotation instanceof FormParam) {
                 final FormParam fd = (FormParam) annotation;
+
                 if (java.io.InputStream.class.isAssignableFrom(constructType(type).getRawClass())) {
-                    final Parameter param = new FormParameter().type("file").name(fd.value());
-                    parameters.add(param);
+                    parameters.add(new FormParameter().type("file").name(fd.value()));
+
                 } else if (javax.servlet.http.Part.class.isAssignableFrom(constructType(type).getRawClass())) {
-                    final Parameter param = new FormParameter().type("file").name(fd.value());
-                    parameters.add(param);
+                    parameters.add(new FormParameter().type("file").name(fd.value()));
+
                 } else {
                     final FormParameter fp = new FormParameter().name(fd.value());
                     final Property schema = ModelConverters.getInstance().readAsProperty(type);
@@ -53,22 +60,11 @@ public class MinijaxSwaggerExtension extends AbstractSwaggerExtension {
             }
         }
 
-        // Only call down to the other items in the chain if no parameters were produced
-        if (parameters.isEmpty()) {
-            parameters = super.extractParameters(annotations, type, typesToSkip, chain);
+        if (!parameters.isEmpty()) {
+            return parameters;
         }
 
-        return parameters;
-    }
-
-    @Override
-    protected boolean shouldIgnoreClass(final Class<?> cls) {
-//        for (final Class<?> item : Arrays.asList(org.glassfish.jersey.media.multipart.FormDataContentDisposition.class,
-//                org.glassfish.jersey.media.multipart.FormDataBodyPart.class)) {
-//            if (item.isAssignableFrom(cls)) {
-//                return true;
-//            }
-//        }
-        return false;
+        // Only call down to the other items in the chain if no parameters were produced
+        return super.extractParameters(annotations, type, typesToSkip, chain);
     }
 }
