59c7b8a271f98034da7ffd75a113490990f32b6e
Cody Ebberson <cody@ebberson.com>
2017-11-14 13:02:55 -0800

    Added alerts and title to View

--- minijax-extensions/minijax-view/src/main/java/org/minijax/view/View.java
+++ minijax-extensions/minijax-view/src/main/java/org/minijax/view/View.java
@@ -1,6 +1,8 @@
 package org.minijax.view;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class View {
@@ -12,11 +14,70 @@ public class View {
         model = new HashMap<>();
     }
 
+    public View(final String templateName, final String title) {
+        this(templateName);
+        model.put("title", title);
+    }
+
     public String getTemplateName() {
         return templateName;
     }
 
     public Map<String, Object> getModel() {
         return model;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Alert> getAlerts() {
+        return ((List<Alert>) model.computeIfAbsent("alerts", k -> new ArrayList<Alert>()));
+    }
+
+    public void addAlert(final Alert alert) {
+        getAlerts().add(alert);
+    }
+
+    public void addSuccessAlert(final String msg) {
+        addAlert(new Alert(AlertType.SUCCESS, msg));
+    }
+
+    public void addInfoAlert(final String msg) {
+        addAlert(new Alert(AlertType.INFO, msg));
+    }
+
+    public void addWarningAlert(final String msg) {
+        addAlert(new Alert(AlertType.WARNING, msg));
+    }
+
+    public void addDangerAlert(final String msg) {
+        addAlert(new Alert(AlertType.DANGER, msg));
+    }
+
+    public enum AlertType {
+        SUCCESS,
+        INFO,
+        WARNING,
+        DANGER
+    }
+
+    public static class Alert {
+        private final AlertType alertType;
+        private final String message;
+
+        public Alert(final AlertType alertType, final String message) {
+            this.alertType = alertType;
+            this.message = message;
+        }
+
+        public AlertType getAlertType() {
+            return alertType;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public String getStyle() {
+            return "alert alert-" + alertType.toString().toLowerCase();
+        }
     }
 }
--- minijax-extensions/minijax-view/src/test/java/org/minijax/view/ViewTest.java
+++ minijax-extensions/minijax-view/src/test/java/org/minijax/view/ViewTest.java
@@ -1,5 +1,41 @@
 package org.minijax.view;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.minijax.view.View.AlertType;
+
 public class ViewTest {
 
+    @Test
+    public void testConstructor() {
+        final View page = new View("foo", "bar");
+        assertEquals("foo", page.getTemplateName());
+        assertEquals("bar", page.getModel().get("title"));
+    }
+
+    @Test
+    public void testGettersSetters() {
+        final View page = new View("foo", "bar");
+        page.addDangerAlert("this is an error");
+        page.addInfoAlert("this is info");
+        page.addWarningAlert("this is a warning");
+        page.addSuccessAlert("this is a success");
+
+        assertEquals(AlertType.DANGER, page.getAlerts().get(0).getAlertType());
+        assertEquals("this is an error", page.getAlerts().get(0).getMessage());
+        assertEquals("alert alert-danger", page.getAlerts().get(0).getStyle());
+
+        assertEquals(AlertType.INFO, page.getAlerts().get(1).getAlertType());
+        assertEquals("this is info", page.getAlerts().get(1).getMessage());
+        assertEquals("alert alert-info", page.getAlerts().get(1).getStyle());
+
+        assertEquals(AlertType.WARNING, page.getAlerts().get(2).getAlertType());
+        assertEquals("this is a warning", page.getAlerts().get(2).getMessage());
+        assertEquals("alert alert-warning", page.getAlerts().get(2).getStyle());
+
+        assertEquals(AlertType.SUCCESS, page.getAlerts().get(3).getAlertType());
+        assertEquals("this is a success", page.getAlerts().get(3).getMessage());
+        assertEquals("alert alert-success", page.getAlerts().get(3).getStyle());
+    }
 }
