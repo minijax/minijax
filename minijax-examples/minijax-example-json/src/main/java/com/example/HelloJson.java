f3b81379bb02dec6eeb4c474e276b54f015b3923
Cody Ebberson <cody@ebberson.com>
2017-11-17 17:20:42 -0800

    Sonar warning

--- minijax-examples/minijax-example-json/src/main/java/com/example/HelloJson.java
+++ minijax-examples/minijax-example-json/src/main/java/com/example/HelloJson.java
@@ -13,6 +13,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.minijax.Minijax;
@@ -23,6 +25,7 @@ import org.minijax.json.JsonFeature;
 public class HelloJson {
 
     @XmlRootElement
+    @XmlAccessorType(XmlAccessType.FIELD)
     public static class Widget {
         String id;
         String value;
