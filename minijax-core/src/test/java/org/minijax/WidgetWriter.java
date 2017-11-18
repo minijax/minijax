75e263b13f0dfa0aa11def70846edcc7b002dd8d
Cody Ebberson <cody@ebberson.com>
2017-11-18 12:33:12 -0800

    Added Widget and WidgetWriter for tests

---
+++ minijax-core/src/test/java/org/minijax/WidgetWriter.java
@@ -0,0 +1,34 @@
+package org.minijax;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+
+public class WidgetWriter implements MessageBodyWriter<Widget> {
+
+    @Override
+    public boolean isWriteable(final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType) {
+        return type == Widget.class;
+    }
+
+    @Override
+    public void writeTo(
+            final Widget widget,
+            final Class<?> type,
+            final Type genericType,
+            final Annotation[] annotations,
+            final MediaType mediaType,
+            final MultivaluedMap<String, Object> httpHeaders,
+            final OutputStream entityStream)
+                    throws IOException, WebApplicationException {
+
+        entityStream.write(String.format("(widget %s %s)", widget.getId(), widget.getValue()).getBytes(StandardCharsets.UTF_8));
+    }
+}
