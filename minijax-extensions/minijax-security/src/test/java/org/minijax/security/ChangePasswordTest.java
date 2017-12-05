67f71d3f7f0dd25eecfe0f8b9133320ad936fda6
Cody Ebberson <cody@ebberson.com>
2017-12-05 11:09:41 -0800

    Added ChangePasswordTest

---
+++ minijax-extensions/minijax-security/src/test/java/org/minijax/security/ChangePasswordTest.java
@@ -0,0 +1,196 @@
+package org.minijax.security;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mindrot.jbcrypt.BCrypt;
+import org.minijax.MinijaxRequestContext;
+import org.minijax.db.DefaultBaseDao;
+import org.minijax.db.PersistenceFeature;
+import org.minijax.test.MinijaxTest;
+
+public class ChangePasswordTest extends MinijaxTest {
+
+    public static class Dao extends DefaultBaseDao implements SecurityDao {
+    }
+
+
+    @Inject
+    private Security<User> security;
+
+
+    @POST
+    @Path("/changepassword")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @RolesAllowed("user")
+    public Response handleChangePassword(
+            @FormParam("oldPassword") final String oldPassword,
+            @FormParam("newPassword") final String newPassword,
+            @FormParam("confirmNewPassword") final String confirmNewPassword) {
+        security.changePassword(oldPassword, newPassword, confirmNewPassword);
+        return Response.ok().build();
+    }
+
+
+    @BeforeClass
+    public static void setUpCsrfFilterTest() throws IOException {
+        register(PersistenceFeature.class);
+        register(new SecurityFeature(User.class, Dao.class));
+        register(ChangePasswordTest.class);
+    }
+
+
+    @Test
+    public void testChangePasswordSuccess() throws IOException {
+        final User user = new User();
+        user.setName("Example 1");
+        user.setEmail("pwd-1@example.com");
+        user.setRoles("user");
+        user.setPassword("my-old-password");
+
+        Cookie cookie = null;
+
+        try (MinijaxRequestContext ctx = createRequestContext()) {
+            ctx.get(Dao.class).create(user);
+            cookie = ctx.get(Security.class).loginAs(user);
+        }
+
+        final Form form = new Form();
+        form.param("csrf", cookie.getValue());
+        form.param("oldPassword", "my-old-password");
+        form.param("newPassword", "my-new-password");
+        form.param("confirmNewPassword", "my-new-password");
+
+        final Response r = target("/changepassword").request().cookie(cookie).post(Entity.form(form));
+        assertNotNull(r);
+        assertEquals(200, r.getStatus());
+
+        try (MinijaxRequestContext ctx = createRequestContext()) {
+            final User check = ctx.get(Dao.class).read(User.class, user.getId());
+            assertFalse(BCrypt.checkpw("my-old-password", check.getPasswordHash()));
+            assertTrue(BCrypt.checkpw("my-new-password", check.getPasswordHash()));
+        }
+    }
+
+
+    @Test
+    public void testUserWithoutPassword() throws IOException {
+        final User user = new User();
+        user.setName("Example 2");
+        user.setEmail("pwd-2@example.com");
+        user.setRoles("user");
+
+        Cookie cookie = null;
+
+        try (MinijaxRequestContext ctx = createRequestContext()) {
+            ctx.get(Dao.class).create(user);
+            cookie = ctx.get(Security.class).loginAs(user);
+        }
+
+        final Form form = new Form();
+        form.param("csrf", cookie.getValue());
+        form.param("oldPassword", "my-old-password");
+        form.param("newPassword", "my-new-password");
+        form.param("confirmNewPassword", "my-new-password");
+
+        final Response r = target("/changepassword").request().cookie(cookie).post(Entity.form(form));
+        assertNotNull(r);
+        assertEquals(400, r.getStatus());
+    }
+
+
+    @Test
+    public void testIncorrectOldPassword() throws IOException {
+        final User user = new User();
+        user.setName("Example 3");
+        user.setEmail("pwd-3@example.com");
+        user.setRoles("user");
+        user.setPassword("my-old-password");
+
+        Cookie cookie = null;
+
+        try (MinijaxRequestContext ctx = createRequestContext()) {
+            ctx.get(Dao.class).create(user);
+            cookie = ctx.get(Security.class).loginAs(user);
+        }
+
+        final Form form = new Form();
+        form.param("csrf", cookie.getValue());
+        form.param("oldPassword", "wrong-old-password");
+        form.param("newPassword", "my-new-password");
+        form.param("confirmNewPassword", "my-new-password");
+
+        final Response r = target("/changepassword").request().cookie(cookie).post(Entity.form(form));
+        assertNotNull(r);
+        assertEquals(400, r.getStatus());
+    }
+
+
+    @Test
+    public void testMismatchedPasswords() throws IOException {
+        final User user = new User();
+        user.setName("Example 4");
+        user.setEmail("pwd-4@example.com");
+        user.setRoles("user");
+        user.setPassword("my-old-password");
+
+        Cookie cookie = null;
+
+        try (MinijaxRequestContext ctx = createRequestContext()) {
+            ctx.get(Dao.class).create(user);
+            cookie = ctx.get(Security.class).loginAs(user);
+        }
+
+        final Form form = new Form();
+        form.param("csrf", cookie.getValue());
+        form.param("oldPassword", "my-old-password");
+        form.param("newPassword", "my-new-password");
+        form.param("confirmNewPassword", "different-password");
+
+        final Response r = target("/changepassword").request().cookie(cookie).post(Entity.form(form));
+        assertNotNull(r);
+        assertEquals(400, r.getStatus());
+    }
+
+
+    @Test
+    public void testPasswordTooShort() throws IOException {
+        final User user = new User();
+        user.setName("Example 5");
+        user.setEmail("pwd-5@example.com");
+        user.setRoles("user");
+        user.setPassword("my-old-password");
+
+        Cookie cookie = null;
+
+        try (MinijaxRequestContext ctx = createRequestContext()) {
+            ctx.get(Dao.class).create(user);
+            cookie = ctx.get(Security.class).loginAs(user);
+        }
+
+        final Form form = new Form();
+        form.param("csrf", cookie.getValue());
+        form.param("oldPassword", "my-old-password");
+        form.param("newPassword", "foo");
+        form.param("confirmNewPassword", "foo");
+
+        final Response r = target("/changepassword").request().cookie(cookie).post(Entity.form(form));
+        assertNotNull(r);
+        assertEquals(400, r.getStatus());
+    }
+}
