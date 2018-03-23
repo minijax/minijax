c8e788110c5a3aa4dfa00154a5ab82c5f00f49cb
Cody Ebberson <cody@ebberson.com>
2018-03-22 17:52:02 -0700

    ImplicitTemplateMap test coverage

---
+++ minijax-core/src/test/java/org/minijax/uri/ImplicitTemplateMapTest.java
@@ -0,0 +1,34 @@
+package org.minijax.uri;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImplicitTemplateMapTest {
+
+    @Test
+    public void testSimple() {
+        final ImplicitTemplateMap map = new ImplicitTemplateMap(new Object[] {"a", "b"});
+        assertEquals("a", map.get("foo"));
+        assertEquals("a", map.get("foo"));
+    }
+
+    @Test
+    public void testEquals() {
+        final ImplicitTemplateMap m1 = new ImplicitTemplateMap(new Object[] {"a", "b"});
+        final ImplicitTemplateMap m2 = new ImplicitTemplateMap(new Object[] {"a", "b"});
+        final ImplicitTemplateMap m3 = new ImplicitTemplateMap(new Object[] {"c", "d"});
+        final ImplicitTemplateMap m4 = new ImplicitTemplateMap(new Object[] {"c", "d"});
+
+        assertEquals(m1, m1);
+        assertEquals(m1.hashCode(), m2.hashCode());
+        assertEquals(m1, m2);
+        assertEquals(m3, m4);
+        assertNotEquals(m1, m3);
+        assertNotEquals(m1, null);
+        assertNotEquals(m1, new Object());
+
+        m4.get("foo");
+        assertNotEquals(m3, m4);
+    }
+}
