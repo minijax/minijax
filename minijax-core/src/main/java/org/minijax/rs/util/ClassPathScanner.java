5de326e4ceb70833dbc705adfe351f96216a3e8c
Cody Ebberson <cody@ebberson.com>
2021-01-03 11:07:54 -0800

    Now using ClassGraph for classpath scanning

--- minijax-core/src/main/java/org/minijax/rs/util/ClassPathScanner.java
+++ minijax-core/src/main/java/org/minijax/rs/util/ClassPathScanner.java
@@ -1,97 +1,54 @@
 package org.minijax.rs.util;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
+
 /**
- * Utilities to scan the classpath in search of classes in a specific package.
+ * Scans package names for classes to be automatically registered.
  *
- * See:  https://stackoverflow.com/a/22462785/2051724
+ * Scanning implemented with the "ClassGraph" library:
+ * https://github.com/classgraph/classgraph
  */
 public class ClassPathScanner {
     private static final Logger LOG = LoggerFactory.getLogger(ClassPathScanner.class);
-    private final Set<Class<?>> result = new HashSet<>();
+    private static final String[] AUTO_SCAN_CLASSES = {
+            "jakarta.ws.rs.ApplicationPath",
+            "jakarta.ws.rs.Path",
+            "jakarta.ws.rs.ext.Provider",
+            "jakarta.websocket.server.ServerEndpoint"
+    };
 
-    public static Set<Class<?>> scan(final String packageName) throws IOException {
-        return scan(packageName, Thread.currentThread().getContextClassLoader());
+    ClassPathScanner() {
+        throw new UnsupportedOperationException();
     }
 
-    public static Set<Class<?>> scan(final String packageName, final ClassLoader cld)
-            throws IOException {
-        final ClassPathScanner scanner = new ClassPathScanner();
-        scanner.scanImpl(packageName, cld);
-        return scanner.result;
-    }
+    public static Set<Class<?>> scan(final String... packageNames) throws IOException {
+        final Set<Class<?>> result = new HashSet<>();
+        try (final ScanResult scanResult =
+                new ClassGraph()
+                    .enableClassInfo()
+                    .enableAnnotationInfo()
+                    .acceptPackages(packageNames)
+                    .scan()) {
 
-    private void scanImpl(final String packageName, final ClassLoader classLoader) throws IOException {
-        final Enumeration<URL> resources = classLoader.getResources(packageName.replace('.', '/'));
-
-        while (resources.hasMoreElements()) {
-            final URL url = resources.nextElement();
-            final String protocol = url.getProtocol();
-
-            if (protocol.equals("file")) {
-                checkDirectory(classLoader, new File(URLDecoder.decode(url.getPath(), StandardCharsets.UTF_8)), packageName);
-            } else if (protocol.equals("jar")) {
-                checkJarFile(classLoader, url, packageName);
-            } else {
-                throw new IllegalArgumentException("Unrecognized classpath protocol: " + protocol);
-            }
-        }
-    }
-
-    private void checkDirectory(final ClassLoader classLoader, final File file, final String name) {
-        if (!file.exists()) {
-            return;
-        }
-
-        if (file.isDirectory()) {
-            for (final File child : file.listFiles()) {
-                checkDirectory(classLoader, child, name + "." + child.getName());
-            }
-
-        } else if (file.getName().endsWith(".class")) {
-            addClass(classLoader, name.substring(0, name.length() - 6));
-        }
-    }
-
-    private void checkJarFile(final ClassLoader classLoader, final URL url, final String pckgname) throws IOException {
-        final JarURLConnection conn = (JarURLConnection) url.openConnection();
-        final JarFile jarFile = conn.getJarFile();
-        final Enumeration<JarEntry> entries = jarFile.entries();
-
-        while (entries.hasMoreElements()) {
-            final JarEntry jarEntry = entries.nextElement();
-
-            String name = jarEntry.getName();
-
-            if (name.endsWith(".class")) {
-                name = name.substring(0, name.length() - 6).replace('/', '.');
-
-                if (name.startsWith(pckgname)) {
-                    addClass(classLoader, name);
+            for (final String annotation : AUTO_SCAN_CLASSES) {
+                for (final ClassInfo classInfo : scanResult.getClassesWithAnnotation(annotation)) {
+                    try {
+                        result.add(Class.forName(classInfo.getName()));
+                    } catch (final ClassNotFoundException | NoClassDefFoundError ex) {
+                        LOG.error("Class error: {}", ex.getMessage(), ex);
+                    }
                 }
             }
         }
-    }
-
-    private void addClass(final ClassLoader classLoader, final String className) {
-        try {
-            result.add(classLoader.loadClass(className));
-        } catch (final ClassNotFoundException | NoClassDefFoundError ex) {
-            LOG.error("Class error: {}", ex.getMessage(), ex);
-        }
+        return result;
     }
 }
